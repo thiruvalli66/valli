27bdb95004e7969d608397a31c1a8f437c9f3253
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-07-27 12:38:32 +0530

    Update and rename large.java to large1.java

--- program/large1.java
+++ program/large1.java
@@ -3,7 +3,7 @@ import java.lang.*;
 import java.io.*;
 
 /* Name of the class has to be "Main" only if the class is public. */
-class large
+class large1
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
