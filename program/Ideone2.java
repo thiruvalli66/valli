6654795eafc2b43e1ca97b4569905fcaf0b3cb40
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-07-27 12:13:51 +0530

    Create Ideone2.java

---
+++ program/Ideone2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		char c;
+		Scanner sc=new Scanner(System.in);
+		c=sc.next().charAt(0);
+		if( (c >= 'a' && c<= 'z') || (c >= 'A' && c <= 'z'))
+		{
+			System.out.println("Alphabet");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
