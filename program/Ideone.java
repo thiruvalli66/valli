bb264472df81e58792a66431f1f5837d8da338f9
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-07-27 10:45:19 +0530

    Create Ideone.java

---
+++ program/Ideone.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+		if(n>0)
+		{
+			System.out.println("postive");
+		}
+		else if(n==0)
+		{
+		System.out.println("zero");
+		}
+		else if(n<0)
+		{
+		System.out.println("negative");
+		}
+	}
+}
