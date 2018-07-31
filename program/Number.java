338593c0a52c6ec2201650ed0661ed197cfe18df
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-07-31 10:03:49 +0530

    Create Number.java

---
+++ program/Number.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Number
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a=2,b=3;
+		double result=Math.pow(a,b);
+		System.out.println("Answer is:" +result);
+	}
+}
