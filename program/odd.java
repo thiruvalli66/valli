dd55ce10fff21ccb5fe249f77052d10b3910ea0d
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-07-31 11:30:26 +0530

    Create odd.java

---
+++ program/odd.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class odd
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i;
+		for(i=1; i<6; i++)
+		{
+			if(i!=1)
+			{
+				if(i%2!=0)
+				{
+					System.out.println(i+" ");
+			}
+	}
+}
+}
+}
