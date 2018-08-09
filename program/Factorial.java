16ba4bfb842840e41a2476d401c1eddfab1ef829
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-09 12:01:04 +0530

    Create Factorial.java

---
+++ program/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Factorial
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int num=5;
+		long Factorial = 1;
+		for(int i=1;i<=num;++i)
+		{
+			//Factorial = factorial *i;
+			Factorial *= i;
+		}
+		System.out.printf("Factorial of %d=%d",num,Factorial);
+		}
+}
