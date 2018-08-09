ed2d3e39a58cae6c22d4f4e6c9d21eec9f406291
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-09 10:33:00 +0530

    Create intervals.java

---
+++ program/intervals.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+		for(int i=n+1;i<q;i++)
+		{
+			if(i % 2 == 0)
+			{
+				System.out.println(i+" ");
+			}
+		}
+	}
+}
