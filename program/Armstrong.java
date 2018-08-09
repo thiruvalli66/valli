2016ccb18619167cc395ecd79334a62013f64252
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-09 11:08:10 +0530

    Create Armstrong.java

---
+++ program/Armstrong.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Armstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int num=371,number,temp,total=0;
+		number=num;
+		while(number != 0)
+		{
+			temp=number % 10;
+			total = total+temp*temp*temp;
+			number /=10;
+		}
+		if(total==num)
+		System.out.println(num + "is an Armstrong number");
+		else
+		System.out.println(num + "is not an armstrong number");
+	}
+}
