e46983358cc487464a6d89f7e3fcf1f7a024b3b8
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-13 12:09:18 +0530

    Create countt.java

---
+++ program1/countt.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class countt
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int i,a,count=0;
+		String str;
+		Scanner s=new Scanner(System.in);
+		str=s.nextLine();
+		for(i=0;i<str.length();i++)
+		{
+			if(str.charAt(i)==' ')
+			count++;
+		}
+		System.out.println(count);
+	}
+}
