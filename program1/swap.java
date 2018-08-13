3c4373b023768df0667c3f738b62dd073f5c3989
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-13 12:35:20 +0530

    Create swap.java

---
+++ program1/swap.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class swap
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int a,b,temp;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextInt();
+		b=sc.nextInt();
+		temp=a;
+		a=b;
+		b=temp;
+		System.out.println(a+" "+b);
+	}
+}
