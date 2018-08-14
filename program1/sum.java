18b2dce8821b471245afc141afc3a764e602b15b
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-14 12:41:30 +0530

    Create sum.java

---
+++ program1/sum.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class sum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n1,n2;
+		Scanner sc=new Scanner(System.in);
+		n1=sc.nextInt();
+		n2=sc.nextInt();
+		int result=n1+n2;
+		System.out.println(result);
+	}
+}
