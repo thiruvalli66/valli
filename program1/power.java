df4486694d744fdaef8cc167f087ab344374f50e
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-14 12:44:56 +0530

    Create power.java

---
+++ program1/power.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class power
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n%2==0)
+		System.out.println("yes");
+		else if(n%2==1)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}
