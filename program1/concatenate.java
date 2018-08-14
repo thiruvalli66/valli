dc84f8efa1911656b899a3dcbb6631f724ceb764
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-14 12:04:34 +0530

    Create concatenate.java

---
+++ program1/concatenate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class concatenate
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+		 String c=a+b;
+		 System.out.println(c);
+	}
+}
