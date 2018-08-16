58e8cb1035cf5c967075962c7dbb63382a430d20
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-16 10:25:33 +0530

    Create one.java

---
+++ program2/one.java
@@ -0,0 +1,36 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class one
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		if(n==1)
+		System.out.println("One");
+		 else if(n==2)
+		System.out.println("Two");
+		 else if(n==3)
+		System.out.println("Three");
+		 else if(n==4)
+		System.out.println("Four");
+		 else if(n==5)
+		System.out.println("Five");
+		 else if(n==6)
+		System.out.println("Six");
+		 else if(n==7)
+		System.out.println("Seven");
+		 else if(n==8)
+		System.out.println("Eight");
+		 else if(n==9)
+		System.out.println("Nine");
+		 else if(n==10)
+		System.out.println("Ten");
+
+	}
+}
