d5af300413111340852847a26e98ce2c00026ff5
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-16 10:58:25 +0530

    Create true.java

---
+++ program2/true.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class true
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String string = "yes";
+        boolean numeric = true;
+
+        try {
+                Double num = Double.parseDouble(string);
+            }    catch (NumberFormatException e)
+        {
+            numeric = false;
+        }
+
+        if(numeric)
+            System.out.println(string + " ");
+        else
+            System.out.println(string + " ");
+	}
+}
