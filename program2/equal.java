ebab0bacc777c4bd0295755aad1607b6e6238ea7
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-16 10:46:15 +0530

    Create equal.java

---
+++ program2/equal.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class equal
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,m;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		m=n-1;
+		if(m%2==0)
+		System.out.println(m);
+		else
+		System.out.println(n);
+
+	}
+}
