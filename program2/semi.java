5dbbff4ee607c192dd711354c0df7b8f3837bce7
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-24 10:45:04 +0530

    Create semi.java

---
+++ program2/semi.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class semi
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		String ch;
+		Scanner sc=new Scanner(System.in);
+		ch=sc.nextLine();
+		if(System.out.printf(ch)==null)
+		{
+			sc.close();
+		}
+	}
+}
