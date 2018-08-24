de6dcabb0291402852811518c8aabc37757792ed
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-24 10:54:11 +0530

    Create near.java

---
+++ program2/near.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class near 
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		n=(n/10)*10;
+		n +=10;
+		System.out.println(n);
+
+	}
+}
