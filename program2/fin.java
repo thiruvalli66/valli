34707b3eeb785f540ed3d0a7f84adb21bc37f12d
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-24 11:04:16 +0530

    Create fin.java

---
+++ program2/fin.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class fin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,m,res;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a=n*2;
+		System.out.println(a);
+	}
+}
