3c5aec1646b0cd45ce84088f44fda9a70ff9ea1c
thiruvalli66 <41734510+thiruvalli66@users.noreply.github.com>
2018-08-29 11:32:59 +0530

    Create sca9.java

---
+++ program3/sca9.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class sca9
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		int n,i;
+	int a[]=new int[10];
+	Scanner sc=new Scanner(System.in);
+	n=sc.nextInt();
+	for(i=0;i<n;i++)
+	{
+		a[i]=sc.nextInt();
+	}
+	for(i=0;i<n;i++)
+	{
+		System.out.println(a[i]+" "+i);
+	}
+	}
+}
